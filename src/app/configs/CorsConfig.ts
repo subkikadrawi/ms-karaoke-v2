f1613fd4c89ff99cbe007337fb41b077ae0f77db
subki <subki@pinjamduit.co.id>
2025-10-10 00:28:21 +0700

    feat: adding whitelist IP

--- src/app/configs/CorsConfig.ts
+++ src/app/configs/CorsConfig.ts
@@ -1,3 +1,9 @@
-const whitelistCors = ['', 'http://localhost:5173', 'http://localhost:5500'];
+const whitelistCors = [
+  '',
+  'http://localhost:5173',
+  'http://localhost:5500',
+  'http://103.127.138.140:5175',
+  'http://103.127.138.140:5175/'
+];
 
-export {whitelistCors};
+export { whitelistCors };
